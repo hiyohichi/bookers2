130865cee4f60fbffeb2fd4f5639ada3dc2839ed
EC2 Default User <ec2-user@ip-172-31-22-80.ap-northeast-1.compute.internal>
2023-11-19 10:36:02 +0000

    2-1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+  #  アクセス制限
+  before_action :is_matching_login_user,only: [:edit,:update]
+  
+  def index
+    @users=User.all
+  end
+
+  def show
+    @user=User.find(params[:id])
+    @books=@user.books.all
+  end
+
+  def edit
+    @user=User.find(params[:id])
+  end
+  
+  def update
+    @user=User.find(params[:id])
+    @user.update
+    redirect_to user_path
+  end 
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name,:profile_image,:introduction)
+  end
+  
+  def is_matching_login_user
+    user=User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end 
+  end 
+  
+end
