bc43699e240cf258a17879ade126a5a023e7ff85
EC2 Default User <ec2-user@ip-172-31-22-80.ap-northeast-1.compute.internal>
2023-11-24 09:34:45 +0000

    7-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
 
   has_many :books,dependent: :destroy
   
-  validates :name,length: {minimum:2}
+  validates :name,length: {in:2..20}, uniqueness: true
+  validates :introduction,length: {maximum:50}
   
   has_one_attached :profile_image
   
